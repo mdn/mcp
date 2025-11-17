c12d02c14795e28a9d0273c7ac3fa67736d07809
Leo McArdle <leo@mca.is>
2025-11-17 13:27:08 +0000

    feat: add search tool (#18)

---
+++ test/tools/search.test.js
@@ -0,0 +1,111 @@
+/* eslint-disable jsdoc/reject-any-type */
+import assert from "node:assert/strict";
+import { after, before, describe, it } from "node:test";
+
+import { MockAgent, setGlobalDispatcher } from "undici";
+
+import searchResultEmpty from "../fixtures/search-result-empty.json" with { type: "json" };
+import searchResult from "../fixtures/search-result.json" with { type: "json" };
+import { createClient, createServer } from "../helpers/client.js";
+
+describe("search tool", () => {
+  /** @type {Awaited<ReturnType<createServer>>} */
+  let server;
+  /** @type {Awaited<ReturnType<createClient>>} */
+  let client;
+  /** @type {import("undici").MockPool} */
+  let mockPool;
+
+  before(async () => {
+    server = await createServer();
+    client = await createClient(server.port);
+
+    const agent = new MockAgent();
+    setGlobalDispatcher(agent);
+    // only allow unmocked requests to the mcp server:
+    agent.enableNetConnect(`localhost:${server.port}`);
+    mockPool = agent.get("https://developer.mozilla.org");
+  });
+
+  it("should return results", async () => {
+    mockPool
+      .intercept({
+        path: "/api/v1/search?q=test",
+        method: "GET",
+      })
+      .reply(200, searchResult);
+
+    /** @type {any} */
+    const { content } = await client.callTool({
+      name: "search",
+      arguments: {
+        query: "test",
+      },
+    });
+    /** @type {string} */
+    const text = content[0].text;
+    assert.ok(
+      text.includes("/en-US/docs/Web/XML/EXSLT/Reference/regexp/test"),
+      "includes result url",
+    );
+    assert.ok(text.includes("# regexp:test()"), "includes result title");
+    assert.ok(
+      text.includes(
+        "regexp:test() tests to see whether a string matches a specified regular expression.",
+      ),
+      "includes result summary",
+    );
+  });
+
+  it("should gracefully handle no results", async () => {
+    const query = "testempty";
+    mockPool
+      .intercept({
+        path: `/api/v1/search?q=${query}`,
+        method: "GET",
+      })
+      .reply(200, searchResultEmpty);
+
+    /** @type {any} */
+    const { content } = await client.callTool({
+      name: "search",
+      arguments: {
+        query,
+      },
+    });
+    /** @type {string} */
+    const text = content[0].text;
+    assert.ok(text.includes(query), "response includes query");
+    assert.ok(
+      text.toLowerCase().includes("no results"),
+      "response mentions no results",
+    );
+  });
+
+  it("should gracefully handle server error", async () => {
+    const query = "error";
+    mockPool
+      .intercept({
+        path: `/api/v1/search?q=${query}`,
+        method: "GET",
+      })
+      .reply(502);
+
+    /** @type {any} */
+    const { content } = await client.callTool({
+      name: "search",
+      arguments: {
+        query,
+      },
+    });
+    /** @type {string} */
+    const text = content[0].text;
+    assert.ok(text.includes("502"), "response includes error code");
+    assert.ok(text.includes(query), "response includes query");
+    assert.ok(text.includes("try again"), "response suggests next action");
+  });
+
+  after(() => {
+    server.listener.close();
+  });
+});
