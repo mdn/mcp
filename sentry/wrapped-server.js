a18c581151febecf7b6d16d6ae19d6e16c900b7f
Leo McArdle <leo@mca.is>
2025-12-09 12:57:43 +0000

    fix: update mcp sdk and fix type errors  (#43)
    * update @modelcontextprotocol/sdk from 1.21.1 to 1.24.3
    
    * fix types

--- sentry/wrapped-server.js
+++ sentry/wrapped-server.js
@@ -5,27 +5,23 @@ import { NonSentryError } from "./error.js";
 
 export class SentryMcpServer extends McpServer {
   /**
-   * @template {import("zod").ZodRawShape} InputArgs
-   * @template {import("zod").ZodRawShape} OutputArgs
-   * @param {Parameters<typeof McpServer.prototype.registerTool<InputArgs, OutputArgs>>} args
+   * @type {McpServer["registerTool"]}
    */
-  registerTool(...args) {
-    const [name, config, callback] = args;
-
-    const wrappedCallback =
-      /** @type {Parameters<typeof McpServer.prototype.registerTool<InputArgs, OutputArgs>>[2]} */ (
-        async (...callbackArgs) => {
-          Sentry.getCurrentScope().setTransactionName(`tool ${name}`);
-          try {
-            return await callback(...callbackArgs);
-          } catch (error) {
-            if (!(error instanceof NonSentryError)) {
-              Sentry.captureException(error);
-            }
-            throw error;
+  registerTool(name, config, callback) {
+    const wrappedCallback = /** @type {typeof callback} */ (
+      async (/** @type {Parameters<typeof callback>} */ ...callbackArgs) => {
+        Sentry.getCurrentScope().setTransactionName(`tool ${name}`);
+        try {
+          // @ts-expect-error: ts can't seem to handle passing through args like this
+          return await callback(...callbackArgs);
+        } catch (error) {
+          if (!(error instanceof NonSentryError)) {
+            Sentry.captureException(error);
           }
+          throw error;
         }
-      );
+      }
+    );
 
     return super.registerTool(name, config, wrappedCallback);
   }
