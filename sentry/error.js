be9aa9f249622e8e1b25867b66c0c7ddc2744fd9
Leo McArdle <leo@mozilla.com>
2026-03-02 16:30:13 +0000

    add silentError glean event and send when we capture a non-sentry error

--- sentry/error.js
+++ sentry/error.js
@@ -1,1 +1,10 @@
-export class NonSentryError extends Error {}
+export class NonSentryError extends Error {
+  /**
+   * @param {string} message
+   * @param {string} gleanReason
+   */
+  constructor(message, gleanReason) {
+    super(message);
+    this.gleanReason = gleanReason;
+  }
+}
